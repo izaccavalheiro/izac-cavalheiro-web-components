8039d21c263acf057a155ad847b1c2a5a0f4fbee
Izac Cavalheiro <izac@izaccavalheiro.com>
2022-10-18 22:56:30 -0300

    Remove data layer from ic-social-network Remove url fallback on ic-social-network

--- src/components/ic-social-network/ic-social-network.tsx
+++ src/components/ic-social-network/ic-social-network.tsx
@@ -7,10 +7,6 @@ import {
   State
 } from '@stencil/core'
 
-import {
-  data
-} from './data'
-
 @Component({
   tag: 'ic-social-network',
   styleUrl: 'ic-social-network.css',
@@ -32,7 +28,7 @@ export class IcSocialNetwork {
     const name = this.name.toLowerCase()
     const image = (this.image || this.defaultImage.replace('NAME', name).replace('-COLOR', this.darkMode ? '-inverted' : ''))
     const imageAlt = `${this.name} logo`
-    const url = this.url || data.filter(filteredItem => filteredItem.name === name)[0].url
+    const url = this.url || ''
     const urlTitle = this.urlTitle || `${this.name} link`
     const target = this.target || this.defaultTarget
 
